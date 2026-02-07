aeb454bd5b07001e363611af1f41b85b792566e1
arfinmahmudshiblu <arfinmahmudshiblu687@gmail.com>
2026-02-07 23:27:50 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Arfin Mahmud",
+    author_email="a.mahmud.cu94@gmail.com",
+    packages=find_packages()
+)
