8209a409430bdfabba6d45ca5f665941eb5e56c2
arfinmahmudshiblu <arfinmahmudshiblu687@gmail.com>
2026-04-21 20:13:31 +0600

    data ingestion added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,26 +1,26 @@
-# import os
-# from datetime import date
+import os
+from datetime import date
 
-# DATABASE_NAME = "US_VISA"
+DATABASE_NAME = "US_VISA"
 
-# COLLECTION_NAME = "visa_data"
+COLLECTION_NAME = "visa_data"
 
-# MONGODB_URL_KEY = "MONGODB_URL"
+MONGODB_URL_KEY = "MONGODB_URL"
 
-# PIPELINE_NAME: str = "usvisa"
-# ARTIFACT_DIR: str = "artifact"
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
 
-# MODEL_FILE_NAME = "model.pkl"
+MODEL_FILE_NAME = "model.pkl"
 
 
-# TARGET_COLUMN = "case_status"
-# CURRENT_YEAR = date.today().year
-# PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
-# FILE_NAME: str = "usvisa.csv"
-# TRAIN_FILE_NAME: str = "train.csv"
-# TEST_FILE_NAME: str = "test.csv"
-# SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+FILE_NAME: str = "usvisa.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
 # AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
@@ -28,14 +28,14 @@
 # REGION_NAME = "us-east-1"
 
 
-# """
-# Data Ingestion related constant start with DATA_INGESTION VAR NAME
-# """
-# DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
-# DATA_INGESTION_DIR_NAME: str = "data_ingestion"
-# DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-# DATA_INGESTION_INGESTED_DIR: str = "ingested"
-# DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
 
 
--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -1,10 +1,10 @@
-# from dataclasses import dataclass 
+from dataclasses import dataclass 
 
 
-# @dataclass
-# class DataIngestionArtifact:
-#     trained_file_path:str 
-#     test_file_path:str 
+@dataclass
+class DataIngestionArtifact:
+    trained_file_path:str 
+    test_file_path:str 
 
 
 
